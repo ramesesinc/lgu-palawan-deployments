ce6b07867dd551f8b4a0d026cc4b129f14981b21
Jessie S. Zamora <jzamss@gmail.com>
2020-06-08 09:00:30 +0800

    RPTIS: 255-03021 update

---
+++ _res/patch/mysql/v255-20200608.001.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS `assessmentnotice_online` (
+  `objid` varchar(50) NOT NULL,
+  `state` varchar(25) NOT NULL,
+  `reftype` varchar(25) NOT NULL,
+  `refid` varchar(50) NOT NULL,
+  `refno` varchar(50) NOT NULL,
+  `refdate` date NOT NULL,
+  `orno` varchar(25) DEFAULT NULL,
+  `ordate` date DEFAULT NULL,
+  `oramount` decimal(16,2) DEFAULT NULL,
+  PRIMARY KEY (`objid`),
+  KEY `ix_state` (`state`) USING BTREE,
+  KEY `ix_refid` (`refid`) USING BTREE,
+  KEY `ix_refno` (`refno`) USING BTREE,
+  KEY `ix_orno` (`orno`) USING BTREE,
+  CONSTRAINT `assessmentnotice_online_ibfk_1` FOREIGN KEY (`objid`) REFERENCES `assessmentnotice` (`objid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+
